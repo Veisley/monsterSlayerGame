56eea8c82557b76c4cb15cb7c4ccc7a684cd6a7a
Görkem Tümkaya <hakasestein@hotmail.com>
2021-08-03 00:39:17 +0300

    First Implementations
    + Character component and attack logic implemented
    - Unnecessary files deleted

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,79 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<template >
+  <template v-if="!gameOver">
+    <Character :character="monster" />
+    <Character :character="player" />
+    <button @click="playerAttack">Basic Attack</button> 
+    <!-- <button @click="specialAttack">Special Attack</button> 
+    <button @click="heal">Self Heal</button> 
+    <button @click="dodge">Dodge</button>  -->
+  </template>
+  <template v-else>
+    <h1>Game Over</h1>
+    <button @click="player.health = 100; monster.health = 100">
+      Restart The Game
+    </button>
+  </template>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+
+<script>
+import Character from "./components/Character.vue";
+import { ref, watch } from "vue";
+export default {
+  components: { Character },
+
+  setup() {
+    const player = ref({ name: "Human", health: 100 });
+    const monster = ref({ name: "Monster", health: 100 });
+    const gameOver = ref(false);
+    const attack = (character, max, min) => {
+      const hitPoint = Math.random() * (max - min) + min;
+
+      if (character.value.health - hitPoint > 0)
+        character.value.health -= hitPoint;
+      else character.value.health = 0;
+    };
+
+    const playerAttack = () => {
+      attack(monster, 25, 10);
+      attack(player, 35, 10);
+    };
+
+    watch(player.value, (newValue, oldValue) => {
+      if (player.value.health == 0) {
+        console.log("player h: " + player.value.health);
+        gameOver.value = true;
+      } else {
+        gameOver.value = false;
+      }
+    });
+
+    return {
+      playerAttack,
+      player,
+      monster,
+      gameOver,
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  background: grey;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Character.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="outer-box">
+    <span class="character-name">
+      {{ character.name }}
+    </span>
+
+    <div class="health-border">
+      <div class="health" :style="{'width':character.health + '%'}"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  props: ["character"],
+};
+</script>
+
+<style>
+section {
+  width: 90%;
+  max-width: 40rem;
+  margin: auto;
+}
+.outer-box {
+  text-align: center;
+  padding: 0.5rem;
+  margin: 1rem auto;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  background: rgb(170, 170, 170);
+  border-radius: 12px;
+}
+.character-name {
+  font-weight: bold;
+}
+
+.health-border {
+  width: 90%;
+  border: solid 2px black;
+  background-color: seashell;
+  border-radius: 3px;
+  margin: 5px auto;
+}
+.health {
+  width: 100%;
+  height: 20px;
+  background: rgb(63, 2, 2);
+}
+</style>
